66666c2bdedd9c4a3690ce5c3661cade1fa5edc4
jraami <jaakko.raami@aalto.fi>
2019-05-04 14:01:24 +0300

    new version

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (!process.env.NODE_ENV) {
+if (!process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const url = process.env.MONGODB_URI
